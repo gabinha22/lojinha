b89bd5502da5b4e4b619a61b34bbdb74b5e04350
gabinha22 <99849719+gabinha22@users.noreply.github.com>
2022-05-16 21:30:13 -0300

    commit inicial

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: 'Deck Azaros',
+        productNumber: '8792',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Deck Boros',
+        productNumber: '872',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Card Blainmaster',
+        productNumber: '293',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Deck Comandante Azul',
+        productNumber: '1000',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Ficha Vampiro',
+        productNumber: '9872',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Box Wonder Spider',
+        productNumber: '87',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Deck Moonlight',
+        productNumber: '9992',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
+   
